6ca6aa7974cda90b5d783ab47b91f0793a98d4d2
patrikbraborec <patrikbraborec@gmail.com>
2022-04-13 16:49:39 +0200

    Add new metrics

--- data-management/src/computations.py
+++ data-management/src/computations.py
@@ -16,7 +16,9 @@ def compute_sma(metric_name, frames):
         )
     )
     indexed_df = indexed_df['first_metric'].to_frame()
-    indexed_df[metric_name + '_sma5'] = indexed_df['first_metric'].rolling(5).mean()
+    indexed_df[metric_name + '_sma30'] = indexed_df['first_metric'].rolling(30).mean()
+    indexed_df[metric_name + '_cma'] = indexed_df['first_metric'].expanding().mean()
+    indexed_df[metric_name + '_ema30'] = indexed_df['first_metric'].ewm(span=30).mean()
     indexed_df.dropna(inplace=True)
     return indexed_df
 
@@ -31,7 +33,9 @@ def store_sma(indexed_df, stock_symbol, metric_name):
     database_conn.autocommit = True
     cur = database_conn.cursor()
     for index, row in indexed_df.iterrows():
-        insert = 'INSERT INTO stocks_technical_analysis (symbol, date, sma5) VALUES (\'{0}\',\'{1}\',{2})'.format(stock_symbol, index.date(), row[metric_name + '_sma5'])
+        insert = 'INSERT INTO stocks_technical_analysis (symbol, date, sma30, cma, ema30) VALUES (\'{0}\',\'{1}\',{2}, {3}, {4})'.format(
+            stock_symbol, index.date(), row[metric_name + '_sma30'], row[metric_name + '_cma'],
+            row[metric_name + '_ema30'])
         cur.execute(insert)
     cur.close()
 
